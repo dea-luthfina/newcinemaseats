2795e251508bb131ac742667c3ae5e88d0698bf2
Dea Luthfina A <103049405+entropiest@users.noreply.github.com>
2022-04-15 12:47:14 +0700

    Add files via upload

---
+++ Core/Human.java
@@ -0,0 +1,107 @@
+package Core;
+public class Human {
+   // data
+    private String name;
+    private String phone;
+    private String amount;
+    private String title;
+    private String day;
+    private String time;
+    private String total;
+    private String payment;
+    private String seats;
+    private String studio;
+    
+
+    // name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // phone
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    // amount
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    // title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    // day
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    // time
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    // total
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+   
+    // payment
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    // seats
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
+
+    // studio
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+    
+}
+
---
+++ Core/Payment.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Core;
+
+/**
+ *
+ * @author Prio
+ */
+public class Payment{
+   public String payment() {
+   return "Payment method choosen ";
+ }
+}
